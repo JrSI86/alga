86e5c436c0bff85317750f2e3a0352413bd17d42
Valdeci Alves <valdeci.alves@hammerconsult.com.br>
2020-01-03 17:47:06 -0300

    Commit security Jwt.

---
+++ src/main/java/com/example/algamoneyapi/config/BasicSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.algamoneyapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+//@Profile("basic-security")
+@Configuration
+@EnableWebSecurity
+public class BasicSecurityConfig extends ResourceServerConfigurerAdapter  {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/categorias").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.csrf().disable();
+	}
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
+	}
+	
+}
